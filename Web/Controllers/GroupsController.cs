8817ea381831ca73f54429d3ec60db4b5f3e61aa
Dmitrii Ershov <dmitryershov0@gmail.com>
2021-02-26 07:37:26 +0300

    Add to group + comments

--- Web/Controllers/GroupsController.cs
+++ Web/Controllers/GroupsController.cs
@@ -19,22 +19,50 @@ namespace Web.Controllers
 		{
 			_service = service;
 		}
-
+		/// <summary>
+		/// Find groups
+		/// </summary>
+		/// <param name="name">Group name</param>
+		/// <returns></returns>
 		[HttpGet("[action]")]
 		public async Task<IEnumerable<GroupView>> FindByName(string name)
 			=> await _service.FindByName(name);
 
+		/// <summary>
+		/// Get all groups
+		/// </summary>
+		/// <returns></returns>
 		[HttpGet("[action]")]
 		public async Task<IEnumerable<GroupView>> GetAll()
 			=> await _service.GetAll();
 
+		/// <summary>
+		/// Delete group
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
 		[HttpGet("[action]")]
 		public async Task<bool> Delete(Guid id)
 			=> await _service.Delete(id);
 
+		/// <summary>
+		/// Create or update group
+		/// </summary>
+		/// <param name="item"></param>
+		/// <returns></returns>
 		[HttpPost("[action]")]
 		public async Task<GroupView> CreateUpdate(GroupUpdateView item)
 			=> await _service.CreateUpdate(item);
+
+		/// <summary>
+		/// Add student to group
+		/// </summary>
+		/// <param name="studentId"></param>
+		/// <param name="groupId"></param>
+		/// <returns></returns>
+		[HttpGet("[action]")]
+		public async Task AddToGroup(Guid studentId, Guid groupId)
+			=> await _service.AddStudent(studentId, groupId);
 
 	}
 }
--- Web/Controllers/StudentsController.cs
+++ Web/Controllers/StudentsController.cs
@@ -20,18 +20,39 @@ namespace Web.Controllers
 			_service = service;
 		}
 
+		/// <summary>
+		/// Get all students
+		/// </summary>
+		/// <returns></returns>
 		[HttpGet("[action]")]
 		public async Task<IEnumerable<StudentView>> GetAll()
 			=> await _service.GetAll();
 
+		/// <summary>
+		/// Delete student
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
 		[HttpGet("[action]")]
 		public async Task<bool> Delete(Guid id)
 			=> await _service.Delete(id);
 
+		/// <summary>
+		/// Create or update student
+		/// </summary>
+		/// <param name="item"></param>
+		/// <returns></returns>
 		[HttpPost("[action]")]
 		public async Task<StudentView> CreateUpdate(StudentCreateView item)
 			=> await _service.CreateUpdate(item);
 
+		/// <summary>
+		/// Search student
+		/// </summary>
+		/// <param name="text">search text</param>
+		/// <param name="page">page number</param>
+		/// <param name="pageSize">page size</param>
+		/// <returns></returns>
 		[HttpGet("[action]")]
 		public async Task<IEnumerable<StudentView>> Search(string text, int page, int pageSize)
 			=> await _service.Search(text, page, pageSize);
